93f747bd8e301b3707f308da26eb094b88234266
minhthi <thi.nm2488@sinhvien.hoasen.edu.vn>
2019-01-14 18:05:29 +0700

    Làm trang tạo phim

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/movie/create', function(req, res, next) {
+  res.render('movie/create', { title: 'Create movie' });
+});
 
 module.exports = router;
